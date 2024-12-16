db21023664a9f471826cf54d36df49b4c1cfef7c
shaliniprabha1920 <shaliniprabha1993@gmail.com>
2024-12-16 21:57:52 +0530

    Replaced model class to Record

--- src/main/java/com/sse_demo/sse_demo/StockData.java
+++ src/main/java/com/sse_demo/sse_demo/StockData.java
@@ -1,15 +1,6 @@
 package com.sse_demo.sse_demo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
 
-@Data
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class StockData {
+public record StockData(int credit_rating) {
 
-    @JsonProperty("credit_rating")
-    private int credit_rating;
 }
